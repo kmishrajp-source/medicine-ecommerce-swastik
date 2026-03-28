5f972fb6a330a4240baeee36c18b377d12b08d61
kmishrajp-source <kmishrajp@gmail.com>
2026-03-28 18:42:25 +0200

    cleanup: remove canary route

--- app/[locale]/canary/page.js
+++
@@ -1,8 +0,0 @@
-export default function CanaryPage() {
-    return (
-        <div style={{ padding: '50px', textAlign: 'center' }}>
-            <h1>Canary Page</h1>
-            <p>If you see this, basic Next.js is working on Vercel.</p>
-        </div>
-    );
-}
